bb4a4aacd90654bc3f5a1b2843aaeb5acfdb6f20
harish-1github <harishkarthick.in@gmail.com>
2026-03-10 13:28:43 +0530

    Initial commit

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int partition(vector<int>& arr, int left, int right) {
+    int pivot = arr[right];
+
+    int i = left - 1;
+    for (int j = left; j < right; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i],arr[left]);
+        }
+    }
+    swap(arr[i+1],arr[right]);
+    return i+1;
+}
+
+void quick_sort(vector<int>& arr, int left, int right){
+    if (left >= right) return;
+
+    int pi = partition(arr, left, right);
+    quick_sort(arr, left, pi-1);
+    quick_sort(arr, pi+1, right);
+}
